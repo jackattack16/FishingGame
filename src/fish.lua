a7545460e71be661cd2e2339fbc500622614b1da
jackson diekmann <jacksondiekmann365@gmail.com>
2026-09-11 11:45:15 -0500

    add scoring function and weight calculation using RNG

--- src/fish.lua
+++ src/fish.lua
@@ -5,10 +5,10 @@ Fish._index = Fish
 -- Create a constructor of sorts for fish types from the FishTypes table
 function Fish:new(fishType)
 	local defualts = FishTypes[fishType]
-
 	local fish = {
 		name = defualts.name,
-		weight = defualts.weight,
+		weight = RNG:random(defualts.min_weight, defualts.max_weight),
+		base_price = defualts.base_price,
 		multiplier = defualts.multiplier,
 		rarity = defualts.rarity,
 	}
@@ -17,17 +17,25 @@ function Fish:new(fishType)
 	return fish
 end
 
+function Fish:getValue()
+	return self.weight * self.base_price
+end
+
 -- Define species / types of fish
-FishTypes = {
+FishType = {
 	grouper = {
 		name = "Grouper",
-		weight = 3,
+		min_weight = 3,
+		max_weight = 15,
+		base_price = 0.5,
 		multiplier = 1,
 		rarity = 1,
 	},
 	mackerel = {
 		name = "Mackerel",
-		weight = 1,
+		min_weight = 1,
+		max_weight = 5,
+		base_price = 0.75,
 		multiplier = 1,
 		rarity = 1,
 	},
