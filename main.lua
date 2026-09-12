2f00c2aad832ff246d0750dd9fb3159b8c08d192
jackson diekmann <jacksondiekmann365@gmail.com>
2026-09-12 16:47:19 -0500

    add simple counter

--- main.lua
+++ main.lua
@@ -4,9 +4,18 @@ end
 function love.load()
 	RNG = love.math.newRandomGenerator() -- create  a global rng machine
 	RNG:setSeed(os.time()) -- set the seed so runs can be reproduced in the future
+
+	fish_to_catch = 5
 end
 
 function love.update(dt) end
+
+-- Calls the code below every time a key is presed
+function love.keypressed(key, scancode, isrepeat)
+	if key == "space" then
+		fish_to_catch = fish_to_catch - 1
+	end
+end
 
 function love.draw()
 	local background = love.graphics.newImage("assets/background.png")
@@ -14,5 +23,6 @@ function love.draw()
 	local background_y_scale = love.graphics.getHeight() / background:getHeight()
 
 	love.graphics.draw(background, 0, 0, 0, background_x_scale, background_y_scale)
-	love.graphics.print("FishingGame", 24, 24)
+	love.graphics.print('FishingGame, press space to "catch" a fish', 24, 24)
+	love.graphics.print("Fish left: " .. fish_to_catch, 25, love.graphics.getHeight() - 50)
 end
