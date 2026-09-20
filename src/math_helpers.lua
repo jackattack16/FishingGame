e59fefa931911f90b28be4270a4b510443576ab5
jackson diekmann <jacksondiekmann365@gmail.com>
2026-09-19 21:32:53 -0500

    update math round function to provide decimals

--- src/math_helpers.lua
+++ src/math_helpers.lua
@@ -1,7 +1,12 @@
 local M = {}
 
-function M.round(n)
-	return math.floor(n + 0.5)
+function M.round(num, numDecimalPlaces)
+	if not numDecimalPlaces then
+		return math.floor(num + 0.5)
+	else
+		local mult = 10 ^ (numDecimalPlaces or 0)
+		return math.floor(num * mult + 0.5) / mult
+	end
 end
 
 return M
